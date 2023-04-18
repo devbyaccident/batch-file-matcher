3453fed7207da9cc0488e1c7ec8219505fed26c7
devbyaccident <blackdenc@chop.edu>
2023-04-18 09:45:11 -0400

    Adding gitignore and fixing chatgpt's busted code

--- src/md5_hash.py
+++ src/md5_hash.py
@@ -13,8 +13,9 @@ def get_s3_file_md5(bucket_name, s3_key):
     # Get the size of the file so we know when to stop reading
     response = s3.head_object(Bucket=bucket_name, Key=s3_key)
     file_size = response['ContentLength']
+    file_read_completely = False
 
-    while True:
+    while file_read_completely == False:
         # Read the file in chunks to reduce memory usage
         end_byte = min(start_byte + chunk_size, file_size) - 1
         response = s3.get_object(
@@ -23,9 +24,16 @@ def get_s3_file_md5(bucket_name, s3_key):
             Range=f'bytes={start_byte}-{end_byte}'
         )
         data = response['Body'].read()
+        # breakpoint()
         if not data:
             break
         md5.update(data)
         start_byte += chunk_size
-    
+        file_read_completely = start_byte >= end_byte
+
     return md5.hexdigest()
+
+bucket_name = "d3b-devops-bucket"
+s3_key = "scripts/deploy_scripts/entrypoint.sh"
+md5_hash = get_s3_file_md5(bucket_name, s3_key)
+print(md5_hash)
